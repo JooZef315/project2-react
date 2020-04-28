be71108b6bc0db704ec5353e2fde0723c8d4a10a
Joozef <m.youssef315@yahoo.com>
2020-04-29 00:07:50 +0200

    bf4

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -27,7 +27,7 @@ const minLength = (len) => (val) => val && (val.length >= len);
 
     handleSubmit(values) {
       this.toggleModal();
-      this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+      this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
     }
 
     render() {
@@ -88,7 +88,7 @@ const minLength = (len) => (val) => val && (val.length >= len);
   }
 
 
-  function RenderComments({comments, addComment, dishId}){
+  function RenderComments({comments, postComment, dishId}){
     if(comments != null)
     return(
       <div className="col-12 col-md-5 m-1">
@@ -110,7 +110,7 @@ const minLength = (len) => (val) => val && (val.length >= len);
           </ul>
         </Card>
         <div>
-          <CommentForm dishId={dishId} addComment={addComment}/>
+          <CommentForm dishId={dishId} postComment={postComment}/>
        </div>
 
       </div>
@@ -179,7 +179,7 @@ const minLength = (len) => (val) => val && (val.length >= len);
             <div className="row">
               <RenderDish dish={props.dish} />
               <RenderComments comments={props.comments}
-                addComment={props.addComment}
+                postComment={props.postComment}
                 dishId={props.dish.id} />
             </div>
           </div>
