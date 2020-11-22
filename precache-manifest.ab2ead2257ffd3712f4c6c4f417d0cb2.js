b8011aa38dc6645b2df0bf8b4b03c63decac59b3
Joozef <m.youssef315@yahoo.com>
2020-11-22 19:28:44 +0200

    Updates

---
+++ precache-manifest.ab2ead2257ffd3712f4c6c4f417d0cb2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31c3594a84e0aa56c317fd4dfbc495c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5256d3908fe9837579d2",
+    "url": "/static/css/2.44298d54.chunk.css"
+  },
+  {
+    "revision": "c35fb24f108362e7f62c",
+    "url": "/static/css/main.29327c19.chunk.css"
+  },
+  {
+    "revision": "5256d3908fe9837579d2",
+    "url": "/static/js/2.f19d39c7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f19d39c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c35fb24f108362e7f62c",
+    "url": "/static/js/main.d8f510ca.chunk.js"
+  },
+  {
+    "revision": "2dfbfffd00575044283b",
+    "url": "/static/js/runtime-main.e05b0089.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
