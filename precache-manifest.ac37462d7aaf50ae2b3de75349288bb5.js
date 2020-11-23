86764089ab1f5765d1b1505d049ea7584516c168
Joozef <m.youssef315@yahoo.com>
2020-11-23 02:17:47 +0200

    Updates

---
+++ precache-manifest.ac37462d7aaf50ae2b3de75349288bb5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a063d5e8472a5f4e9730213ea9ced73c",
+    "url": "/Fusion-Restaurant/index.html"
+  },
+  {
+    "revision": "15470599dd4bd5583542",
+    "url": "/Fusion-Restaurant/static/css/2.afdaea60.chunk.css"
+  },
+  {
+    "revision": "2a7a93f487ed76004fb2",
+    "url": "/Fusion-Restaurant/static/css/main.29327c19.chunk.css"
+  },
+  {
+    "revision": "15470599dd4bd5583542",
+    "url": "/Fusion-Restaurant/static/js/2.80989a56.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Fusion-Restaurant/static/js/2.80989a56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a7a93f487ed76004fb2",
+    "url": "/Fusion-Restaurant/static/js/main.42c0d07f.chunk.js"
+  },
+  {
+    "revision": "ce19f29c84ba5ff9a06d",
+    "url": "/Fusion-Restaurant/static/js/runtime-main.759daebf.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Fusion-Restaurant/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Fusion-Restaurant/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Fusion-Restaurant/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Fusion-Restaurant/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Fusion-Restaurant/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7869b6769a3dc024f785c59bd9cc8e4a.js"
+  "/Fusion-Restaurant/precache-manifest.ac37462d7aaf50ae2b3de75349288bb5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Fusion-Restaurant/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
